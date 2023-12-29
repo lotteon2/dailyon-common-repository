851ed05a190ad4483964cb89c3d5e4c1ab404d22
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-29 12:35:06 +0900

    [FIX] fix kakfa topic from enum to interface

--- src/main/java/dailyon/domain/common/KafkaTopic.java
+++ src/main/java/dailyon/domain/common/KafkaTopic.java
@@ -1,22 +1,14 @@
 package dailyon.domain.common;
 
-import lombok.Getter;
+public interface KafkaTopic {
 
-@Getter
-public enum KafkaTopic {
-  APPROVE_PAYMENT("approve-payment"),
-  CANCEL_ORDER("cancel-order"),
-  CREATE_ORDER("create-order"),
-  CREATE_ORDER_PRODUCT("create-order-product"),
-  CREATE_ORDER_USE_COUPON("create-order-use-coupon"),
-  CREATE_REVIEW("create-review"),
-  USE_MEMBER_POINTS("use-member-points"),
-  CREATE_MEMBER_FOR_SNS("create-member-for-sns"),
-  UPDATE_MEMBER_FOR_SNS("update-member-for-sns");
-
-  private final String value;
-
-  KafkaTopic(String value) {
-    this.value = value;
-  }
+  String APPROVE_PAYMENT = "approve-payment";
+  String CANCEL_ORDER = "cancel-order";
+  String CREATE_ORDER = "create-order";
+  String CREATE_ORDER_PRODUCT = "create-order-product";
+  String CREATE_ORDER_USE_COUPON = "create-order-use-coupon";
+  String CREATE_REVIEW = "create-review";
+  String USE_MEMBER_POINTS = "use-member-points";
+  String CREATE_MEMBER_FOR_SNS = "create-member-for-sns";
+  String UPDATE_MEMBER_FOR_SNS = "update-member-for-sns";
 }
